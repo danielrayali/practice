9d72c09dc4353e6342b68dd489e1127fab3e8b15
Daniel Ali <daniel.ray.ali@gmail.com>
2020-08-02 23:25:06 -0400

    Another wrong implementation of d4s2

--- adventofcode/d4s2.cc
+++ adventofcode/d4s2.cc
@@ -1,33 +1,46 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
 
 using namespace std;
 
 bool IsPassword(int number) {
     string num_str = to_string(number);
+    // cout << "Processing string: " << num_str << endl;
     int prev = -1;
     bool double_found = false;
-    int repeat = 0;
     for (size_t i = 0; i < num_str.size(); ++i) {
         int cur = num_str[i];
         if (cur < prev) {
             return false;
         } else if (cur == prev) {
             double_found = true;
-            repeat++;
-            prev = cur;
-            continue;
         }
-        if ((repeat > 0) && (repeat & 0x1) == 0) {
-            return false;
-        }
-        repeat = 0;
         prev = cur;
     }
-    if ((repeat > 0) && (repeat & 0x1) == 0) {
-        return false;
-    }
     return double_found;
+}
+
+bool ContainsEvenRepeats(int number) {
+    string num_str = to_string(number);
+    int prev = -1, cur = 0;
+    int repeats = 0;
+    for (size_t i = 0; i < num_str.size(); ++i) {
+        cur = static_cast<int>(num_str[i]);
+        if (prev == cur) {
+            repeats++;
+            prev = cur;
+            continue;
+        } else if ((repeats > 0) && ((repeats & 0x1) == 0)) {
+            return true;
+        }
+        repeats = 0;
+        prev = cur;
+    }
+    if ((repeats > 0) && ((repeats & 0x1) == 0)) {
+        return true;
+    }
+    return false;
 }
 
 int main(int argc, char* argv[]) {
@@ -45,12 +58,20 @@ int main(int argc, char* argv[]) {
     in >> end;
 
     cout << "Processing from " << start << " to " << end << endl;
-    int num_passwords = 0;
+    vector<int> old_passwords;
     for (int i = start; i < end; ++i) {
         if (IsPassword(i)) {
-            cout << "Password found: " << i << ". ";
-            cout << "Num of passwords: " << num_passwords << endl;
+            cout << "Password found: " << i << endl;
+            old_passwords.push_back(i);
+        }
+    }
+
+    int num_passwords = 0;
+    for (auto password : old_passwords) {
+        if (!ContainsEvenRepeats(password)) {
             num_passwords++;
+        } else {
+            cout << "Password eliminated: " << password << endl;
         }
     }
 
