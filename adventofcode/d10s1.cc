5111ebfb2773ef66d512914641126d81e607ffe3
Daniel Ali <daniel.ray.ali@gmail.com>
2020-09-04 10:21:40 -0400

    Implemented correct d10s1

--- adventofcode/d10s1.cc
+++ adventofcode/d10s1.cc
@@ -2,18 +2,21 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
-#include <unordered_set>
+#include <set>
 #include <cmath>
 
 using namespace std;
 
 struct Point {
-    float x;
-    float y;
+    int x = 0;
+    int y = 0;
 };
 
-float CalculateSlope(const Point first, const Point second) {
-    return sqrt(pow((second.x - first.x), 2.0f) + pow((second.y - first.y), 2.0f));
+int CalculateGCD(const int a, const int b) {
+    if (b == 0) {
+        return a;
+    }
+    return CalculateGCD(b, a % b);
 }
 
 int main(int argc, char* argv[]) {
@@ -30,26 +33,34 @@ int main(int argc, char* argv[]) {
         if (temp == '\n') {
             asteroids.emplace_back();
         } else if (temp == '#') {
-            asteroids.back().emplace_back(true);
+            asteroids.back().push_back(true);
         } else {
-            asteroids.back().emplace_back(false);
+            asteroids.back().push_back(false);
         }
     }
 
     size_t max_unique_slopes = 0;
     for (int x_left = 0; x_left < asteroids.size(); ++x_left) {
         for (int y_left = 0; y_left < asteroids.at(x_left).size(); ++y_left) {
-            Point left;
-            left.x = x_left;
-            left.y = y_left;
-            unordered_set<float> unique_slopes;
+            if (!asteroids.at(x_left).at(y_left)) {
+                continue;
+            }
+
+            set<pair<int,int>> unique_slopes;
             for (int x_right = 0; x_right < asteroids.size(); ++x_right) {
                 for (int y_right = 0; y_right < asteroids.at(x_right).size(); ++y_right) {
-                    Point right;
-                    right.x = x_right;
-                    right.y = y_right;
-                    float slope = CalculateSlope(left, right);
-                    unique_slopes.insert(slope);
+                    if (!asteroids.at(x_right).at(y_right)) {
+                        continue;
+                    }
+
+                    if ((x_left == x_right) && (y_left == y_right)) {
+                        continue;
+                    }
+
+                    int x_diff = x_left - x_right;
+                    int y_diff = y_left - y_right;
+                    int gcd = CalculateGCD(abs(x_diff), abs(y_diff));
+                    unique_slopes.insert({x_diff / gcd, y_diff / gcd});
                 }
             }
             if (unique_slopes.size() > max_unique_slopes) {
