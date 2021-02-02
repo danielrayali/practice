b335a96f93c23ab48597f9157659e6b4e6def909
Daniel Ali <daniel.ray.ali@gmail.com>
2021-02-01 22:17:36 -0500

    Implemented (most) of the CRT day 13 star 2

--- adventofcode/2020/d13s2.cc
+++ adventofcode/2020/d13s2.cc
@@ -2,8 +2,73 @@
 #include <fstream>
 #include <vector>
 #include <cstdlib>
+#include <algorithm>
 
 using namespace std;
+
+// Represents a congruence of the form x ≡ a (mod b) where "a" is the remainder and "b" is the modulus.
+// x is unspecified.
+struct Congruence {
+    long long modulus = -1;
+    long long remainder = -1;
+
+    Congruence(const long long _modulus, const long long _remainder) :
+        modulus(_modulus), remainder(_remainder) {}
+
+    Congruence(const Polynomial& polynomial) :
+        modulus(polynomial.coefficient), remainder(polynomial.constant) {}
+};
+
+// Represents a polynomial of the form y = ax + b. Where "a" is the coefficient and "b" is the constant.
+// x and y are unspecified variables.
+struct Polynomial {
+    long long coefficient = -1;
+    long long constant = -1;
+
+    Polynomial(const long long _coefficient, const long long _constant) :
+        coefficient(_coefficient), constant(_constant) {}
+
+    Polynomial(const Congruence& congruence) :
+        coefficient(congruence.modulus), constant(congruence.remainder) {}
+};
+
+// Shamelessly stolen from this well answered stack overflow question:
+// https://stackoverflow.com/questions/14029516/need-an-efficient-subtraction-algorithm-modulo-a-number
+long long ModuloSubtract(const long long minuend, const long long subtrahend, const long long modulus) {
+    return ((minuend - subtrahend) % modulus) + ((minuend >= subtrahend) ? 0 : modulus);
+}
+
+long long ModuloDivide(const long long dividend, const long long divisor, const long long modulus) {
+    if (divisor == 0) {
+        throw runtime_error("Cannot divide by zero");
+    }
+    // Get the multiplicative inverse of the divisor
+    long long inverse = 1;
+    long long product = divisor;
+    while ((product % modulus) != 1) {
+        inverse++;
+        product = (divisor * inverse);
+    }
+
+    return (inverse * dividend) % modulus;
+}
+
+// Executes a single iteration of the Chinese remainder theorem
+Polynomial CRTIteration(const Polynomial& polynomial, const Congruence& congruence) {
+    long long value = ModuloSubtract(congruence.remainder, polynomial.constant, congruence.modulus);
+    value = ModuloDivide(value, polynomial.coefficient, congruence.modulus);
+    return Polynomial(congruence.modulus, value);
+}
+
+ostream& operator<<(ostream& out, const Congruence& congruence) {
+    out << "x ≡ " << congruence.remainder << " (mod " << congruence.modulus << ")";
+    return out;
+}
+
+ostream& operator<<(ostream& out, const Polynomial& polynomial) {
+    out << "y = " << polynomial.coefficient << "x + " << polynomial.constant;
+    return out;
+}
 
 int main(int argc, char* argv[]) {
     ifstream input("d13s1.txt");
@@ -12,41 +77,39 @@ int main(int argc, char* argv[]) {
     const long long earliest_start = atoll(arrival_str.c_str());
     cout << "Read: " << earliest_start << endl;
 
-    vector<long long> bus_ids;
+    vector<Congruence> system;
+    long long count = 0;
     while (!input.eof()) {
         string bus_id_str;
         getline(input, bus_id_str, ',');
+        cout << "Read: " << bus_id_str << endl;
         if (bus_id_str == "x") {
-            bus_id_str = "1";
+            // bus_id_str = "1";
+            continue;
         }
-        long long bus_id = atoll(bus_id_str.c_str());
-        bus_ids.push_back(bus_id);
-        if (bus_id_str != "1") {
-            cout << "Read: x = " << (bus_ids.size() - 1) << " mod " << bus_id << endl;
-        }
+
+        system.emplace_back(atoll(bus_id_str.c_str()), count);
+        count++;
     }
 
-    long long product = 1;
-    for (auto bus_id : bus_ids) {
-        product *= bus_id;
+    // Sort by descending moduli
+    sort(system.begin(), system.end(), [](const Congruence& a, const Congruence& b) -> bool{
+        return a.modulus > b.modulus;
+    });
+
+    cout << "System of congruences to solve: " << endl;
+    for (auto congruence : system) {
+        cout << "  " << congruence << endl;
     }
 
-    vector<long long> y;
-    for (auto bus_id : bus_ids) {
-        y.push_back(product / bus_id);
+    Polynomial polynomial(system.front());
+    for (int i = 1; i < system.size(); i++) {
+        cout << "iteration " << i << endl;
+        polynomial = CRTIteration(polynomial, system[i]);
     }
 
-    vector<long long> z;
-    for (int i = 0; i < y.size(); ++i) {
-        z.push_back((1/y[i]) % bus_ids[i]);
-    }
-
-    int timestamp = 0;
-    for (int i = 0; i < y.size(); ++i) {
-        timestamp += i * y[i] * z[i];
-    }
-
-    cout << "Timestamp: " << timestamp << endl;
-
+    cout << "Resulting polynomial: " << polynomial << endl;
+    cout << "4 - 6 (mod 5) = " << ModuloSubtract(4, 6, 5) << endl;
+    cout << "3 / 7 (mod 5) = " << ModuloDivide(3, 7, 5) << endl;
     return 0;
 }
